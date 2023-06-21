0a711cb20a0dbe9c542817aa50b3483c46b0f4d1
Yevgeniy <yev.bor@yandex.ru>
2023-06-21 19:20:10 +0300

    updated slider

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -1,31 +1,30 @@
+import { useState } from 'react';
+import dashboardImage1 from '../assets/dashboards/dashboard-1.png';
+import dashboardImage2 from '../assets/dashboards/dashboard-2.png';
+import dashboardImage3 from '../assets/dashboards/dashboard-3.png';
+import '../styles/sliderStyles.css';
 
-import { useState } from "react";
-import dashboardImage1 from "../assets/dashboards/dashboard-1.png";
-import dashboardImage2 from "../assets/dashboards/dashboard-2.png";
-import dashboardImage3 from "../assets/dashboards/dashboard-3.png";
-import "../styles/sliderStyles.css";
-
- const ImageSlider = () => {
-  const slides = [
-    dashboardImage1,
-    dashboardImage2,
-    dashboardImage3,
-  ];
+const ImageSlider = () => {
+  const slides = [dashboardImage1, dashboardImage2, dashboardImage3];
 
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const handleClick = (index:number) => (event: React.MouseEvent<HTMLDivElement>) => {
-    setCurrentIndex(index);
-  };
+  const handleClick =
+    (index: number) => (event: React.MouseEvent<HTMLDivElement>) => {
+      setCurrentIndex(index);
+    };
 
   return (
     <div className="slider-container">
-      <div className="slides" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
+      <div
+        className="slides"
+        style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+      >
         {slides.map((slide, index) => (
           <img
             key={index}
             src={slide}
-            alt='Dashboard'
+            alt="Dashboard"
             className="slide-image"
           />
         ))}
@@ -33,7 +32,11 @@ import "../styles/sliderStyles.css";
       <div className="dots">
         {slides.map((slide, index) => (
           <div key={index} onClick={handleClick(index)}>
-            <span className={`${currentIndex === index ? 'dot dot__active' : 'dot'}`}></span>
+            <span
+              className={`${
+                currentIndex === index ? 'dot dot__active' : 'dot'
+              }`}
+            ></span>
           </div>
         ))}
       </div>
