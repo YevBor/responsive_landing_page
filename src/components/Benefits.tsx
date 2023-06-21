6ea94faf45cd1d2286fd87ca4ceafae46d2a67b0
Yevgeniy <yev.bor@yandex.ru>
2023-06-22 00:46:41 +0300

    refactor

--- src/components/Benefits.tsx
+++ src/components/Benefits.tsx
@@ -1,10 +1,21 @@
 const Benefits = () => (
-    <ul className='sm:flex sm:justify-center self-start px-4 lg:px-0   sm:flex-wrap max-w-md sm:self-center lg:justify-start lg:self-start'>
-        <li className="md:text-lg"><span className="text-gold text-2xl px-2 md:text-2xl">●</span>Monitor unlimited devices</li>
-        <li className="md:text-lg"><span className="text-gold text-2xl px-2 md:text-2xl">●</span>Fixed price</li>
-        <li className="md:text-lg"><span className="text-gold text-2xl px-2 md:text-2xl">●</span>5-minute onboarding</li>
-        <li className="md:text-lg"><span className="text-gold text-2xl px-2 md:text-2xl">●</span>30-day free trial</li>
-    </ul>
+  <ul className="sm:flex sm:justify-center self-start px-4 lg:px-0   sm:flex-wrap max-w-md sm:self-center lg:justify-start lg:self-start">
+    <li className="md:text-lg">
+      <span className="text-gold text-2xl px-2 md:text-2xl">●</span>Monitor
+      unlimited devices
+    </li>
+    <li className="md:text-lg">
+      <span className="text-gold text-2xl px-2 md:text-2xl">●</span>Fixed price
+    </li>
+    <li className="md:text-lg">
+      <span className="text-gold text-2xl px-2 md:text-2xl">●</span>5-minute
+      onboarding
+    </li>
+    <li className="md:text-lg">
+      <span className="text-gold text-2xl px-2 md:text-2xl">●</span>30-day free
+      trial
+    </li>
+  </ul>
 );
 
 export default Benefits;
